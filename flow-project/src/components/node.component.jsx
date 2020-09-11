89748bf52c6776f5501bfb743da3c9bea9f08d45
tayronearaujo <tayron_machado@hotmail.com>
2020-09-11 15:07:18 -0300

    add custom flow

--- flow-project/src/components/node.component.jsx
+++ flow-project/src/components/node.component.jsx
@@ -1,52 +1,52 @@
 import React,{ useState, Fragment}from 'react'
-//import ReactBrand from './../components/reactBrand.png'
-//import AngularBrand from './../components/angularBrand.png'
 import './../components/node.css';
 import ReactFlow, {addEdge,Background, Controls, MiniMap, Handle } from 'react-flow-renderer'
 
 const initialElements = [
-  {id:'1',sourcePosition: 'bottom', type:'input', data:{label:'Seleciona oferta'}, position:{x:250,y:0}, style: { background:'#40ba8d' }},
+  {id:'1',sourcePosition: 'bottom', type:'input', data:{label:(
+    <>
+      <div className="oferta">
+        <h3>Seleciona oferta</h3>
+      </div>
+    </>
+)}, position:{x:250,y:0}, style: { background:'#40ba8d', width: 145, height: 70 }},
   {id: '2' ,sourcePosition: 'top',data: {
     label: ( 
       <>
-          
-            <h3>Financiamento sem risco</h3>
-         
-          <div className="content">
-            <p>Fluxo para financiamento sem necessicade de validação de fraude</p>
-            <div className="cases">
-              <div className="caseIf">
-                <h4>If</h4>
-              </div>
-              <div className="caseElse">
-                <h4>Else</h4>
-              </div>
+        <h3>Financiamento sem risco</h3>
+        <div className="content">
+          <p>Fluxo para financiamento sem necessicade de validação de fraude</p>
+          <div className="cases">
+            <div className="caseIf">
+              <h4>If</h4>
+            </div>
+            <div className="caseElse">
+              <h4>Else</h4>
             </div>
           </div>
-      
+        </div>
         <Handle type="source" position="right" id="a" style={{ top: 90, background: '#000' }} />
         <Handle type="source" position="right" id="b" style={{ bottom: 15, top: 'auto', background: '#000' }} />
       </>
     )
-  ,},position: { x: 0, y: 100 }, style: { background: '#ffd600', width: 180 ,margin: 0 ,padding: 0 }},
+  ,},position: { x: 0, y: 120 }, style: { background: '#ffd600', width: 180 ,margin: 0 ,padding: 0 }},
 
   { id: '3',sourcePosition: 'left',targetPosition: 'left',data: { 
     label: (
       <>
+      <div className="proposta">
         <h3>Efetivar proposta</h3>
         <p>GET</p>
         <p>http://localhost:8080</p>
+      </div>
       </>
   )
-  },position: { x: 400, y: 100 }, style: { background:'#5e5e5e' }},
+  },position: { x: 450, y: 200 }, style: { background:'#2c2c2e', border: '3px solid #2f94bc', height: 70 ,margin:0, padding:0}},
 
-  { id: 'e1-2', source: '1', target: '2', type: 'smoothstep', arrowHeadType: 'arrowclosed'},
-  //{ id: 'e2-3', source: '2', target: '3', type: 'smoothstep', arrowHeadType: 'arrowclosed'},
-  //{ id: 'e2-4', source: '2', target: '4', type: 'smoothstep', arrowHeadType: 'arrowclosed'},
- // { id: 'e3-5', source: '3', target: '5', type: 'smoothstep', animated: true, arrowHeadType: 'arrowclosed'},
-  //{ id: 'e4-5', source: '4', target: '5', type: 'smoothstep', animated: true, arrowHeadType: 'arrowclosed'},
+  { id: 'e1-2', source: '1', target: '2', type: 'smoothstep', arrowHeadType: 'arrowclosed', style: { stroke: '#40ba8d' }},
+  { id: 'e2a-3', source: '2__a', target: '3', type: 'smoothstep', arrowHeadType: 'arrowclosed', animated: true, style: { stroke: '#ffd600' }},
+  { id: 'e2a-4', source: '2__b', target: '3', type: 'smoothstep', arrowHeadType: 'arrowclosed', animated: true, style: { stroke: '#ffd600' }},
   
-
 ]
 
 const onLoad = (ReactFlowInstance) =>{
@@ -78,9 +78,9 @@ const MyNode = () => {
         connectionLineType="belizer"
         snapToGrid={true}
         snapGrid={[16,16]}
-        style={{width:'100%',height:'90vh'}}
+        style={{width:'100%',height:'100vh', background:'#1c1c1e'}}
       >
-        <Background color="#000" gap={16} />
+        <Background  gap={16} />
         <MiniMap 
         nodeColor={n=>{
           if(n.type === 'input') return 'blue'
